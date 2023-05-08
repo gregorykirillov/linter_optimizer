369d67ae89a57a6737d1136f45d8eb7049625644
Gregory <kgn.0@ya.ru>
2023-05-08 13:37:50 +0300

    Added ability to delete correlated patterns

--- script_1.js
+++ script_1.js
@@ -21,8 +21,10 @@ const flattenDirectory = (paths) => {
 }
 
 const checkIsCorrectPath = async (pattern) => {
+  const isGlobalIgnore = () => pattern.slice(0, 1) === '*'
+
   const result = await stylelint.lint({
-    files: pattern,
+    files: isGlobalIgnore() ? `**/${pattern}` : pattern,
     configFile: '.stylelintrc.json',
     ignorePath: 'emptyStyleLintIgnoreFile'
   })
@@ -39,20 +41,72 @@ const getPathsToDelete = async (pattern) => {
   return pathsToDelete
 }
 
-const checkIgnorePattern = async (pattern) => {
-  const isDirectory = pattern.slice(-1) === '/'
-  const newPattern = isDirectory ? `${pattern}*` : pattern
-  const foundFilesPaths = flattenDirectory(glob.sync(newPattern))
-
+const getLintCorrectFilePaths = async (foundFilesPaths, pattern, toBeDeletedPaths) => {
   if (foundFilesPaths.length === 0) {
-    deleteLine(pattern)
-    return
+    return [...toBeDeletedPaths, pattern]
   }
 
-  const pathsToDelete = await getPathsToDelete(pattern)
-  pathsToDelete.forEach((file) => deleteLine(file.replace(/\\/g, '/')))
+  const res = await getPathsToDelete(pattern)
+  return res.values()
 }
 
-ignorePatterns.forEach(async (pattern) => {
-  checkIgnorePattern(pattern)
-})
+const removeCorrelatedPatterns = (patternFilesMap) => {
+  if (patternFilesMap.size === 0) return []
+
+  const pathMap = new Map()
+  for (const [pattern, paths] of patternFilesMap) {
+    for (const path of paths) {
+      if (!pathMap.has(path)) pathMap.set(path, [])
+      pathMap.get(path).push(pattern)
+    }
+  }
+
+  const clearedPatterns = []
+  const correlatedPaths = []
+
+  for (const [pattern, paths] of patternFilesMap) {
+    const isCorrelated = paths.every(filePath =>
+      pathMap.get(filePath).filter(pat => pat !== pattern).length > 0
+    )
+
+    isCorrelated ? correlatedPaths.push(pattern) : clearedPatterns.push(pattern)
+  }
+
+  return { clearedPatterns, correlatedPaths }
+}
+
+const fixPattern = (pattern) => {
+  const isDirectory = () => pattern.slice(-1) === '/'
+  const isGlobalIgnore = () => pattern.slice(0, 1) === '*'
+
+  let newPattern = pattern
+  if (isDirectory()) newPattern = `${pattern}*`
+  else if (isGlobalIgnore()) newPattern = `**/${pattern}`
+
+  return newPattern
+}
+
+const getFilesFromPattern = (globFormatPattern) => {
+  return flattenDirectory(glob.sync(globFormatPattern))
+}
+
+const patternFilesMap = ignorePatterns.reduce((acc, pattern) => {
+  const globFormatPattern = fixPattern(pattern)
+  acc.set(pattern, getFilesFromPattern(globFormatPattern))
+  return acc
+}, new Map())
+
+const main = async () => {
+  const { clearedPatterns, correlatedPaths } = removeCorrelatedPatterns(patternFilesMap)
+  const pathsToBeDeleted = [...correlatedPaths]
+
+  for (const pattern of clearedPatterns) {
+    const foundFilesPaths = patternFilesMap.get(pattern)
+
+    const lintCorrected = await getLintCorrectFilePaths(foundFilesPaths, pattern, pathsToBeDeleted)
+    pathsToBeDeleted.push(...lintCorrected)
+  }
+
+  pathsToBeDeleted.forEach(path => deleteLine(path))
+}
+main()
